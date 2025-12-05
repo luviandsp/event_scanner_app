536b6b18bd82a8f66dbbfeace216047fdcaaab4d
Viannn <luvdasyapra@gmail.com>
2025-12-05 14:31:50 +0700

    refactor: update initial route and replace logo image path

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       title: 'Event Scanner App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPageStaff(),
         '/': (context) => const MainPage(),
